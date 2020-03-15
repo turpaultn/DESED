e22d930ac7df19c3e1554520d06fc40f32649039
nturpaul <nicolas.turpault@inria.fr>
2020-03-15 14:34:19 +0100

    Add pitch shifting, time stretching and snr to add_event

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def package_file(fname):
 
 setup(
     name='desed',
-    version='1.1.6',
+    version='1.1.7',
     description="DESED dataset utils",
     author="Nicolas Turpault, Romain Serizel, Ankit Shah, Justin Salamon",
     author_email="turpaultn@gmail.com",
