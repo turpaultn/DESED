1668c8d88d59d06eac511c779ec2c66ecc1a26c1
nturpaul <nicolas.turpault@inria.fr>
2020-03-17 19:41:04 +0100

    Update Zenodo DESED_synth and comment reverb

--- synthetic/create_dcase2020_dataset.sh
+++ synthetic/create_dcase2020_dataset.sh
@@ -20,7 +20,7 @@ mkdir -p ${DATASET_DIR}
 # If not already installed, install DESED
 pip install desed@git+https://github.com/turpaultn/DESED
 echo "Download and extract soundbank"
-wget -O DESED_synth_soundbank.tar.gz https://zenodo.org/record/3702397/files/DESED_synth_soundbank.tar.gz?download=1
+wget -O DESED_synth_soundbank.tar.gz https://zenodo.org/record/3713328/files/DESED_synth_soundbank.tar.gz?download=1
 tar -xzf DESED_synth_soundbank.tar.gz
 rm DESED_synth_soundbank.tar.gz
 echo "Done"
@@ -33,7 +33,7 @@ echo "Done"
 echo "Getting jams for dcase 2020"
 cd ${DATASET_DIR}
 # Get jams file
-wget -O DESED_synth_dcase20jams.tar.gz https://zenodo.org/record/3702397/files/DESED_synth_dcase20_train_jams.tar.gz?download=1
+wget -O DESED_synth_dcase20jams.tar.gz https://zenodo.org/record/3713328/files/DESED_synth_dcase20_train_jams.tar.gz?download=1
 tar -xzf DESED_synth_dcase20jams.tar.gz
 rm DESED_synth_dcase20jams.tar.gz
 cd ${ROOT_DIR}
@@ -50,32 +50,32 @@ echo "Done"
 
 
 ##########
-# Reverberate with fuss
+# Reverberate with fuss, could be uncommented if needed
 ##########
-echo "Getting RIR data"
-wget -O FUSS_rir_data.tar.gz https://zenodo.org/record/3694384/files/FUSS_rir_data.tar.gz?download=1
-tar -xzf FUSS_rir_data.tar.gz
-#rm FUSS_rir_data.tar.gz
-
-echo "Cloning sound-separation repo"
-git clone https://github.com/google-research/sound-separation.git
-
-echo "RIR coming from fuss"
-SUBSET=train  # Here the subset is also used to define the subset of RIR to use
-INPUT_PATH=${DATASET_DIR}/audio/${SUBSET}/synthetic20
-
-# Reverb default path
-RIR=${ROOT_DIR}/rir_data
-
-REVERB_PATH=${INPUT_PATH}_reverb
-MIX_INFO=${REVERB_PATH}/mix_info.txt
-SRC_LIST=${REVERB_PATH}/src_list.txt
-RIR_LIST=${REVERB_PATH}/rir_list.txt
-
-echo "number of processors is ${NPROC}, you can change it if you want"
-######## Under this line you should not have to change anything ###########
-
-# Reverberate data using same RIR as Google baseline
-python ${SCRIPTS_PATH}/reverberate_data.py --rir_folder=${RIR} --input_folder=${INPUT_PATH} \
---reverb_out_folder=${REVERB_PATH} --rir_subset=${SUBSET} \
---mix_info_file=${MIX_INFO}  --src_list_file=${SRC_LIST} --rir_list_file=${RIR_LIST} --nproc=${NPROC}
+#echo "Getting RIR data"
+#wget -O FUSS_rir_data.tar.gz https://zenodo.org/record/3694384/files/FUSS_rir_data.tar.gz?download=1
+#tar -xzf FUSS_rir_data.tar.gz
+##rm FUSS_rir_data.tar.gz
+#
+#echo "Cloning sound-separation repo"
+#git clone https://github.com/google-research/sound-separation.git
+#
+#echo "RIR coming from fuss"
+#SUBSET=train  # Here the subset is also used to define the subset of RIR to use
+#INPUT_PATH=${DATASET_DIR}/audio/${SUBSET}/synthetic20
+#
+## Reverb default path
+#RIR=${ROOT_DIR}/rir_data
+#
+#REVERB_PATH=${INPUT_PATH}_reverb
+#MIX_INFO=${REVERB_PATH}/mix_info.txt
+#SRC_LIST=${REVERB_PATH}/src_list.txt
+#RIR_LIST=${REVERB_PATH}/rir_list.txt
+#
+#echo "number of processors is ${NPROC}, you can change it if you want"
+######### Under this line you should not have to change anything ###########
+#
+## Reverberate data using same RIR as Google baseline
+#python ${SCRIPTS_PATH}/reverberate_data.py --rir_folder=${RIR} --input_folder=${INPUT_PATH} \
+#--reverb_out_folder=${REVERB_PATH} --rir_subset=${SUBSET} \
+#--mix_info_file=${MIX_INFO}  --src_list_file=${SRC_LIST} --rir_list_file=${RIR_LIST} --nproc=${NPROC}
