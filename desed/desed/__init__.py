e53d4060b8d18dbac51c74ac26b1cc465cdb0c02
nturpaul <nicolas.turpault@inria.fr>
2021-02-25 00:50:53 +0100

    Update download_real so we can call desed package to download the data easily.

--- desed/desed/__init__.py
+++ desed/desed/__init__.py
@@ -1,12 +1,7 @@
-from .download_real import download_real_set
-from .download_soundbank import (
-    download_soundbank,
-    download_zenodo_soundbank,
-    get_backgrounds_train,
-)
+from .download_real import download_audioset_data, download_eval_public
+from .download_soundbank import download_soundbank
 from .generate_synthetic import (
     SoundscapesGenerator,
     generate_files_from_jams,
     generate_tsv_from_jams,
 )
-from .get_backgroung_training import get_background_training
